d01e1ed0bb59cf71191fed0d9e6459f0b99120ab
MAXHUB <MAXHUB@DESKTOP-T6KLQOF>
2023-09-11 08:48:38 +0530

    Created New Class

---
+++ DDF_FrameWork/src/commonFunctions/Method_Primus.java
@@ -0,0 +1,16 @@
+package commonFunctions;
+
+public class Method_Primus {
+public void add()
+{
+	int a=34,b=8,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=567,b=4,c;
+	c=a/b;
+	System.out.println(c);
+}
+}
