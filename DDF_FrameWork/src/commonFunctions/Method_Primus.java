22eedfdfce2bae943aabc83930a5fc50256a9b56
MAXHUB <MAXHUB@DESKTOP-T6KLQOF>
2023-09-11 08:51:16 +0530

    Modified Values

--- DDF_FrameWork/src/commonFunctions/Method_Primus.java
+++ DDF_FrameWork/src/commonFunctions/Method_Primus.java
@@ -3,13 +3,13 @@ package commonFunctions;
 public class Method_Primus {
 public void add()
 {
-	int a=34,b=8,c;
+	int a=54656,b=67,c;
 	c=a+b;
 	System.out.println(c);
 }
 public void div()
 {
-	int a=567,b=4,c;
+	int a=9898,b=2,c;
 	c=a/b;
 	System.out.println(c);
 }
